9920ab3a4679576e331c08671fa592cd84592e11
Melese <melese@Meleses-MacBook-Pro.local>
2024-09-06 19:19:14 +0300

    new feature

--- test.py
+++ test.py
@@ -1,18 +1,12 @@
-from typing import Union
-
-from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
-app = FastAPI()
 
-
-@app.get("/")
-def read_root():
+if __name__ == '__main__':
     page = requests.get("https://www.bankofabyssinia.com/exchange-rate-2/")
     soup = BeautifulSoup(page.text, "html.parser")
     date = soup.css.select_one(".middle_content .row-1 .column-1").string
     buying = soup.css.select_one(".middle_content #tablepress-15 .row-hover .row-4 .column-2").string
     selling = soup.css.select_one(".middle_content #tablepress-15 .row-hover .row-4 .column-3").string
-    obj = {'date': date, 'buying': buying, 'selling': selling}
+    obj ={'date':date,'buying':buying,'selling':selling}
 
-    return obj
+    print(obj)
