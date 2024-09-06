c3a2eb40d4ac8bc2bf9c2cf591b5ccee5052be6c
Melese <melese@Meleses-MacBook-Pro.local>
2024-09-06 20:18:46 +0300

    new feature

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from typing import Union
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "https://birr.netlify.app/"
+]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins,allow_methods=["*"], allow_headers=["*"])
 
 
 @app.get("/")
